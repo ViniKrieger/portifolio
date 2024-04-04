b2405f71c42afafdb1cf8377e7eb70962f43e69a
ViniKrieger <viniheiko@gmail.com>
2024-04-04 18:20:10 -0300

    Adicionando temas e finalizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+
 import Sidebar from './containers/Sidebar'
 import EstiloGlobal, { Container } from './style'
 import Sobre from './containers/Sobre'
 import Projetos from './containers/projetos'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
+import { useState } from 'react'
 
 function App() {
+  const [estaUsandotemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandotemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandotemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
